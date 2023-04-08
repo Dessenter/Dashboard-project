d43033c4c2bacc576f348cf373a08515d32a4f0f
Sindre <mrsnowfire@hotmail.com>
2023-04-08 22:31:50 +0200

    Added Bitcoin and Time.

---
+++ Dashboard/index.js
@@ -0,0 +1,27 @@
+//using scrimba api for now, will change to unsplash once signed up. 
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=travel")
+.then(res => res.json())
+.then(data => {
+    console.log(data.urls.full)
+    document.body.style.backgroundImage=`url("${data.urls.full}")`
+    document.getElementById("author-name").textContent = `By: ${data.user.name}`
+})
+.catch(error => {
+    document.body.style.backgroundImage=`url("https://images.unsplash.com/photo-1505832018823-50331d70d237?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODA5ODE3NTA&ixlib=rb-4.0.3&q=85
+    ")`
+})
+
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+.then(res => res.json())
+.then(data => {
+    document.getElementById("crypto-top").innerHTML = `<img src=${data.image.small} alt="image of bitcoin logo" /> 
+    <span>${data.name}</span>`
+    document.getElementById("crypto-price").textContent = `Current price: ${data.market_data.current_price.nok} NOK`
+    //can remove seconds from the clock by removing "second:"numeric". I just like to see it updating in seconds. 
+    setInterval(() =>{
+        document.getElementById("time-id").textContent = new Date().toLocaleTimeString('en-US', { hour12: true, 
+            hour: "numeric", minute: "numeric",second: "numeric"});
+    },1000)
+    
+})
+.catch(err => console.error(err))
